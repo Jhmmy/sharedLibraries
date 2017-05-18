4f5947894ac943154a09d7c3cc67da4e5680254a
dzlitidis <Dimitrios.Zlitidis@arhs-dev-hellas.com>
2017-05-18 16:04:27 +0300

    New method

--- ZlitLibrary.groovy
+++ ZlitLibrary.groovy
@@ -7,3 +7,8 @@ def sayHelloToName(def name) {
 def sayGoodbyeToName(def name) {
 	println "Goodbye " +name;
 }
+
+def checkout(def repo, def isUseCredentials, def credentialsId){
+	def credentials = isUseCredentials ? credentialsId : ''
+	git url: repo[0][0], branch: [0][1], credentialsId: credentials
+}
